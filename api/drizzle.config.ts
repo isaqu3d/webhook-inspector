21cdd659b464730e50c238d4f5367388225dc40c
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-30 12:41:26 -0300

    style: fix formatting and add missing semicolons in configuration files

--- api/drizzle.config.ts
+++ api/drizzle.config.ts
@@ -4,10 +4,10 @@ import { defineConfig } from "drizzle-kit";
 export default defineConfig({
   dialect: "postgresql",
   dbCredentials: {
-    url: env.DATABASE_URL
+    url: env.DATABASE_URL,
   },
 
-  out: './src/db/migrations',
-  schema: './src/db/schema/index.ts',
-  casing: 'snake_case'
+  out: "./src/db/migrations",
+  schema: "./src/db/schema/index.ts",
+  casing: "snake_case",
 });
