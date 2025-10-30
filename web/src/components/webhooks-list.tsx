4c4486fdf88e249b6a348e7c0192b61a7212dd1b
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-30 18:34:50 -0300

    feat: implement fetching and rendering of webhooks in WebhooksList component

--- web/src/components/webhooks-list.tsx
+++ web/src/components/webhooks-list.tsx
@@ -1,12 +1,28 @@
+import { useSuspenseQuery } from "@tanstack/react-query";
+import { webhookListSchema } from "../http/schemas/webhooks";
 import { WebhooksListItem } from "./webhooks-list-item";
 
 export function WebhooksList() {
+  //const {} = useInfiniteQuery()
+
+  const { data } = useSuspenseQuery({
+    queryKey: ["webhooks"],
+    queryFn: async () => {
+      const response = await fetch("http://localhost:3333/api/webhooks");
+
+      const data = await response.json();
+
+      return webhookListSchema.parse(data);
+    },
+  });
+
   return (
-    <div>
-      <WebhooksListItem />
-      <WebhooksListItem />
-      <WebhooksListItem />
-      <WebhooksListItem />
+    <div className="flex-1 overflow-y-auto">
+      <div className="space-y-1 p-2">
+        {data.webhooks.map((webhook) => {
+          return <WebhooksListItem key={webhook.id} webhook={webhook} />;
+        })}
+      </div>
     </div>
   );
 }
