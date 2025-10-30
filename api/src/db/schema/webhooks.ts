2db9fc3f5f8d6791c1897eae0e6bbf06e13ae113
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-30 12:41:51 -0300

    style: fix formatting and add missing semicolons in configuration files

--- api/src/db/schema/webhooks.ts
+++ api/src/db/schema/webhooks.ts
@@ -2,7 +2,9 @@ import { integer, jsonb, pgTable, text, timestamp } from "drizzle-orm/pg-core";
 import { uuidv7 } from "uuidv7";
 
 export const webhooks = pgTable("webhooks", {
-  id: text().primaryKey().$defaultFn(() => uuidv7()),
+  id: text()
+    .primaryKey()
+    .$defaultFn(() => uuidv7()),
   method: text().notNull(),
   pathname: text().notNull(),
   ip: text().notNull(),
@@ -12,5 +14,5 @@ export const webhooks = pgTable("webhooks", {
   queryParams: jsonb().$type<Record<string, string>>(),
   headers: jsonb().$type<Record<string, string>>().notNull(),
   body: text(),
-  createdAt: timestamp().notNull().defaultNow()
-})
+  createdAt: timestamp().notNull().defaultNow(),
+});
