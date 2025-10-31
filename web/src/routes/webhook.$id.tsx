59dd2562baf44aafe2b2aa49b918bc30ebbfaade
Isaque de Sousa <isaque.error404@gmail.com>
2025-10-31 11:23:14 -0300

    refactor: remove unused query logic and simplify RouteComponent structure

--- web/src/routes/webhook.$id.tsx
+++ web/src/routes/webhook.$id.tsx
@@ -1,8 +1,6 @@
-import { useSuspenseQuery } from "@tanstack/react-query";
 import { createFileRoute } from "@tanstack/react-router";
 import { Suspense } from "react";
 import { WebhookDetails } from "../components/webhook-details";
-import { webhookDetailSchema } from "../http/schemas/webhooks";
 
 export const Route = createFileRoute("/webhook/$id")({
   component: RouteComponent,
@@ -11,41 +9,8 @@ export const Route = createFileRoute("/webhook/$id")({
 function RouteComponent() {
   const { id } = Route.useParams();
 
-  const { data } = useSuspenseQuery({
-    queryKey: ["webhook", id],
-    queryFn: async () => {
-      const response = await fetch(`http://localhost:3333/api/webhooks/${id}`);
-
-      const data = await response.json();
-
-      return webhookDetailSchema.parse(data);
-    },
-  });
-
-  const overviewData = [
-    {
-      key: "Method",
-      value: data.method,
-    },
-
-    {
-      key: "Status Code",
-      value: data.statusCode,
-    },
-
-    {
-      key: "Content-Type",
-      value: data.contentType || "application/json",
-    },
-
-    {
-      key: "Content-Length",
-      value: `${data.contentLength || "0"} bytes`,
-    },
-  ];
-
   return (
-    <Suspense fallback={<div>Loading webhook details...</div>}>
+    <Suspense>
       <WebhookDetails id={id} />
     </Suspense>
   );
